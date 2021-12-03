b93114ed3af380a51018e2c04cde72b9a7fc4d9e
Tomilov Sergey <tomexpert@yandex.ru>
2021-12-03 13:37:21 +0800

    v1.1

--- src/js/button.js
+++ src/js/button.js
@@ -27,6 +27,10 @@ export default class Popovers {
     popoverBody.className = 'popover-body';
     popoverBody.textContent = this.text;
 
+    const arrow = document.createElement('div');
+    arrow.className = 'arrow';
+
+    this.popover.appendChild(arrow);
     this.popover.appendChild(popoverHead);
     this.popover.appendChild(popoverBody);
 
@@ -35,26 +39,42 @@ export default class Popovers {
   }
 
   setPopoverPosition() {
+    const arrow = this.popover.querySelector('.arrow');
     switch (this.host.dataset.popoverPosition) {
       case 'top':
         this.popover.style.top = `${(this.host.offsetTop - this.popover.offsetHeight) - 15}px`;
-        this.popover.style.left = `${this.host.offsetLeft - Math.abs((this.host.offsetWidth - this.popover.offsetWidth) / 2)}px`;
+        this.popover.style.left = `${this.host.offsetLeft + this.host.offsetWidth / 2 - this.popover.offsetWidth / 2}px`;
+        arrow.style.left = 'calc(50% - 10px)';
+        arrow.style.top = `${this.popover.offsetHeight - 3}px`;
+        arrow.classList.add('top');
         break;
       case 'right':
-        this.popover.style.top = `${(this.host.offsetTop - Math.abs((this.host.offsetHeight - this.popover.offsetHeight) / 2))}px`;
+        this.popover.style.top = `${this.host.offsetTop + this.host.offsetHeight / 2 - this.popover.offsetHeight / 2}px`;
         this.popover.style.left = `${this.host.offsetLeft + this.host.offsetWidth + 15}px`;
+        arrow.style.left = '-21px';
+        arrow.style.top = 'calc(50% - 10px)';
+        arrow.classList.add('right');
         break;
       case 'bottom':
         this.popover.style.top = `${this.host.offsetTop + this.host.offsetHeight + 15}px`;
-        this.popover.style.left = `${this.host.offsetLeft - Math.abs((this.host.offsetWidth - this.popover.offsetWidth) / 2)}px`;
+        this.popover.style.left = `${this.host.offsetLeft + this.host.offsetWidth / 2 - this.popover.offsetWidth / 2}px`;
+        arrow.style.left = 'calc(50% - 10px)';
+        arrow.style.top = `${-15}px`;
+        arrow.classList.add('bottom');
         break;
       case 'left':
-        this.popover.style.top = `${(this.host.offsetTop - Math.abs((this.host.offsetHeight - this.popover.offsetHeight) / 2))}px`;
+        this.popover.style.top = `${this.host.offsetTop + this.host.offsetHeight / 2 - this.popover.offsetHeight / 2}px`;
         this.popover.style.left = `${this.host.offsetLeft - this.popover.offsetWidth - 15}px`;
+        arrow.style.left = `${this.popover.offsetWidth - 8}px`;
+        arrow.style.top = 'calc(50% - 10px)';
+        arrow.classList.add('left');
         break;
       default:
         this.popover.style.top = `${(this.host.offsetTop - this.popover.offsetHeight) - 15}px`;
         this.popover.style.left = `${this.host.offsetLeft - Math.abs((this.host.offsetWidth - this.popover.offsetWidth) / 2)}px`;
+        arrow.style.left = 'calc(50% - 10px)';
+        arrow.style.top = `${this.popover.offsetHeight - 3}px`;
+        arrow.classList.add('top');
         break;
     }
   }
